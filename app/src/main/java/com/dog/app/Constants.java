a0705188cdd96ad772deac6800185a681e0425e4
cuiyue <380920705@qq.com>
2017-03-23 17:02:19 +0800

    去掉多余的空行

--- app/src/main/java/com/dog/app/Constants.java
+++ app/src/main/java/com/dog/app/Constants.java
@@ -6,7 +6,6 @@ import java.io.File;
 public class Constants {
 
     //================= PATH ====================
-
     public static final String PATH_DATA = App.getInstance().getCacheDir().getAbsolutePath() + File.separator + "data";
     public static final String PATH_CACHE = PATH_DATA + "/NetCache";
 
